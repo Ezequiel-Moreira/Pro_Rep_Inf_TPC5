243e8ceaa6c4003107b00b7be7624650460632ff
Ezequiel-Moreira <99ezequiel90@gmail.com>
2018-10-19 09:18:04 +0100

    secound commit: adição de estrutura base no servidor

---
+++ server.js
@@ -0,0 +1,10 @@
+var http = require('http')
+var fs = require('fs')
+var url = require('url')
+var server=7000
+
+http.createServer( (req,res) =>{
+
+}).listen(server,()=>{
+  console.log('Servidor á escuta na porta ' + server) 
+})
