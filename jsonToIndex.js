2a8d58e54bec7383e5f0e24732af6f45038076b1
Ezequiel-Moreira <99ezequiel90@gmail.com>
2018-10-20 16:47:14 +0100

    ninth commit: criando um index à mão

--- jsonToIndex.js
+++ jsonToIndex.js
@@ -1,24 +1,18 @@
 const fs = require('fs')
 const folder = './data/json/'
+const dest = './data/index.xml'
 
 fs.readdir(folder,(erro,ficheiros)=>{
-  if(!erro){
-    ficheiros.forEach(ficheiro => {
-      var dadosFicheiro = fs.readFileSync(folder + '/' +ficheiro)
-      var parsedJSON = JSON.parse(dadosFicheiro)
-      var id = parsedJSON._id
-      var titulo = parsedJSON.titulo
-
-      fs.appendFile("./test", id + '\n' + titulo + '\n\n'  ,(erro)=>{
-        if(!erro){
-          console.log("Now go and see the test file")
-        }else{
-          console.log(erro)
-        }
-      })
-
-    });
-  }else{
-    console.log(erro)
-  }  
-})
+  if(erro){console.log(erro)}
+  ficheiros.forEach(ficheiro => {
+    var dadosFicheiro = fs.readFileSync(folder + '/' +ficheiro)
+    var parsedJSON = JSON.parse(dadosFicheiro)
+    var id = parsedJSON._id
+    var titulo = parsedJSON.titulo
+    fs.appendFile(dest,
+    '\t<id>' + id + '</id>' + '\n' + '\t<titulo>' +titulo + '</titulo>\n\n'
+    ,(erro)=>{
+      if(erro){console.log(erro)}        
+    })
+  });
+},console.log('Agora vai ver o ficheiro ' + dest))
